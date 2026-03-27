46e8c000d84c250dfb57823cf4aa85bfa95b72e6
Vlad <vladoniscenko@outlook.com>
2026-03-27 09:24:00 +0100

    feat: add various improvements

---
+++ PrintCraftApi/Migrations/20260327071302_PhoneAndCount.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrintCraftApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhoneAndCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "OrderItems",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "OrderItems");
+        }
+    }
+}
